090367e556cb52fe5d4b73533a5d99b95a9ac8d7
adam-hudak <adamhudak1@proton.me>
2024-05-31 15:06:14 +0200

    feat: Footer

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -8,11 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <div className="flex h-screen flex-col">
-      <main className="flex-1">
-        <Header />
-        {children}
-        <Footer />
-      </main>
+      <Header />
+      <main className="flex-1">{children}</main>
+      <Footer />
     </div>
   );
 }
